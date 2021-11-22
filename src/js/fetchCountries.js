6fea03f9378bc5c9a72fdde6f3741f346fca0a4f
Annomaliiya <ivasyukanna@gmail.com>
2021-11-23 00:34:30 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,9 @@
 'use strict';
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const PARAMS = '?fields=name,capital,population,flags,languages';
 
 export const fetchCountries = (country) => {
-    return fetch(`${BASE_URL}${country}`).then(response => {
+    return fetch(`${BASE_URL}${country}${PARAMS}`).then(response => {
         if (!response.ok) {
             throw new Error(response.status);
         }
